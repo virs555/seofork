e04281fe3f2ca357e4a86eedfcc46a856a308a6c
Alexander P <virs3014@mail.ru>
2020-12-27 22:05:51 +0300

    Вынес константу за класс, поменял private метод на protected, поправил форматирование, убрал лишний return False

--- xmlproxy.py
+++ xmlproxy.py
@@ -3,20 +3,21 @@ import xml.etree.cElementTree as ET
 
 import config
 
+XML_PROXY = 'https://xmlproxy.ru/search/xml'
 
 class XmlParser:
 
     def __init__(self):
         self.params = config.params
-        self.XML_PROXY = 'https://xmlproxy.ru/search/xml'
+        self.xml_proxy = XML_PROXY 
 
-    def __get_xml(self, query, domain, region):
+    def _get_xml(self, query, domain, region):
         params = self.params
         params['query'] = query
         params['lr'] = region
         params['domain'] = domain
         try:
-            response = requests.get(self.XML_PROXY, params=params)
+            response = requests.get(self.xml_proxy, params=params)
             response.raise_for_status()
             return ET.fromstring(response.content)
         except(requests.RequestException):
@@ -26,7 +27,7 @@ class XmlParser:
 
     def get_position(self, query, domain, region='213'):
         query = str(query)
-        xml_doc = self.__get_xml(query, domain, region)
+        xml_doc = self._get_xml(query, domain, region)
         if xml_doc:
             if xml_doc[0][0].tag == 'error':
                 print (f"{xml_doc[0][0].attrib['code']} {xml_doc[0][0].text}")
@@ -43,13 +44,12 @@ class XmlParser:
                     if domain in docs.find('domain').text:
                         result_dic['url_page'] = docs.find('url').text
                         result_dic['position'] = counter
-
                         break
                     else:
                         result_dic['url_page'] = None
                         result_dic['position'] = None
                 return result_dic
-            return False
         return False
-classP = XmlParser()
-print(classP.get_position('купить молоко', 'lenta.com'))
+
+classXML = XmlParser()
+print(classXML.get_position('купить молоко', 'lenta.com'))
